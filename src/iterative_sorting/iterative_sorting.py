be1fd3910cae3ab328c5153f0b72d965fbc2eefd
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-06-03 15:15:32 -0700

    finished iterative sorting questions

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -13,13 +13,29 @@ def selection_sort(arr):
     return arr
 
 
-print(selection_sort([2, 4, 3, 1]))
+# print(selection_sort([2, 4, 3, 1]))
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
+    while True and len(arr) > 1:
+        num_swaps = 0
+        for i in range(len(arr)-1):
+            if arr[i] > arr[i+1]:
+                temp1 = arr[i]
+                temp2 = arr[i+1]
+                arr[i] = temp2
+                arr[i+1] = temp1
+                num_swaps += 1
+        if num_swaps == 0:
+            return arr
+    if len(arr) == 0:
+        return []
+    if len(arr) == 1:
+        return arr
 
-    return arr
+
+# print(bubble_sort([2, 5, 4, 1, 3]))
 
 
 # STRETCH: implement the Count Sort function below
